f9252369e8f6420f0dae14d4d91d2130d46b8d41
K.M.H.H.Karunanayake <hirunihasanka99@gmail.com>
2025-11-13 22:10:00 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,27 @@
+<div class="row-fluid">
+  <div id="footer" class="span12"> <?php echo date("Y");?> &copy; Developed By The Bumz</a> </div>
+</div>
+
+
+
+<style>
+#footer {
+  color: white;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 460px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+</style>
+
+
---
+++ include/head.php
@@ -0,0 +1,8 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
+<link rel="stylesheet" href="css/matrix-style.css" />
+<link rel="stylesheet" href="css/matrix-login.css" />
+<link href="font-awesome/css/fontawesome.css" rel="stylesheet" />
+<link href="font-awesome/css/all.css" rel="stylesheet" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
